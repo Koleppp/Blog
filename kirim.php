34c00d11504466797ac5bd45c4123c341e6c7068
kolep <maulanasyarif460@gmail.com>
2022-06-09 12:32:30 +0700

    Update seluruh file

--- kirim.php
+++ kirim.php
@@ -1,31 +1,20 @@
 <?php
+if(isset($_POST['submit'])){
+  $nama = $_POST['nama'];
+  $email = $_POST['email'];
+  $masukan = $_POST['masukan'];
+  $nowa = $_POST['nowa'];
+  if(!empty($nama)&& !empty($email) && !empty($masukan)){
+    header("location:https://api.whatsapp.com/send?phone=$nowa&text=Nama:%20$nama%20%0DEmail:%20$email%20%0DMasukan:$masukan");
+  }
+  else{
+    
+    echo" <script>
+    alert ('Message failed to send! message dont empty!');
+    </script>
+    ";
+
+  }
+  }
   
-        $tujuan=$_POST['tujuan'];
-        $subjek=$_POST['subjek'];
-        $pesan=$_POST['pesan'];
-
-        include "classes/class.phpmailer.php";
-        $mail = new PHPMailer; 
-        $mail->IsSMTP();
-        $mail->SMTPSecure = 'ssl'; 
-        $mail->Host = "smtp.gmail.com"; //host email
-        $mail->SMTPDebug = 2;
-        $mail->Port = 465;
-        $mail->SMTPAuth = true;
-        $mail->Username = "kolep447@gmail.com"; //user email
-        $mail->Password = "GRANDPRIME"; //password email 
-        $mail->SetFrom("kolep447@gmail.com","Contoh Nama"); //set email pengirim
-        $mail->Subject = $subjek; //subyek email
-        $mail->AddAddress($tujuan);  // email tujuan
-        $mail->MsgHTML($pesan); //pesan
-
-
-        if($mail->Send()){
-          echo "<script>alert('Kirim Pesan Sukses')</script>";
-          echo "<meta http-equiv='refresh' content='0; url=index.php'>";
-        }else
-          echo "<script>alert('GAGAL')</script>";
-          
-    ?>
-
-<!-- Elseif Channel -->
+?>
